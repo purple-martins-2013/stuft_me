72ec88c9a9f7fc903350109fb5d8c7287e3eb544
Apprentice <apprentice@devbootcamp.com>
2013-08-28 10:06:52 -0700

    first commit with rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuftMe::Application.config.secret_key_base = 'c97927ecadc86ea66ccaa3cadf49d788b51a876b388ad3dc8d64c65574fe4987f35379bff9dd038c2cc164e0604d591f3d68b01bd3babe4f260ad3a623af8fea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StuftMe::Application.config.session_store :cookie_store, key: '_stuft_me_session'
