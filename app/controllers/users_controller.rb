d79e54d45396a828234e447413eeb23e730f29d4
Apprentice <apprentice@devbootcamp.com>
2013-08-29 15:13:21 -0700

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @plates = Plate.find_all_by_user_id(@user.id)
-
-    p '*' * 88
-    p params
-    p "user"
-    p @user
-    p "plates"
-    p @plates
   end
 
   def index
