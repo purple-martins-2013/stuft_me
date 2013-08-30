e8e9eaa0f42bdbdd048e0b0544455eaf67819018
Apprentice <apprentice@dbc21.local>
2013-08-30 14:25:47 -0700

    make rspec test for the Sesions Controller post action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe SessionsController do
-
+  describe 'POST #create' do
+    before { request.env["omniauth.auth"] = omniauth_credentials }
+    let(:omniauth_credentials) { {"provider" => "instagram", "uid" => "1234567", "info" => { "name" => "test-user"}} }
+    
+    context 'When ommiauth.auth is invalid' do
+      let(:omniauth_credentials) { {provider: nil, uid: nil, :info['name'] => nil }}  
+      it "should raise an error " do
+        # request.env["omniauth"]["provider"] = nil   
+        expect { post :create}.to raise_error
+      end
+    end
+  end
 end
