1e8d75dbddce8695e9274ee581c55b7df5e8af1c
Jeff Chang <jeffchang.ca@gmail.com>
2013-08-30 03:14:31 -0700

    add default for location if empty

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -6,10 +6,16 @@ class Plate < ActiveRecord::Base
   validates_uniqueness_of :url
 
   before_create :fill_description
+  before_create :fill_location
 
   def fill_description
     self.description = "Unnamed" if description == ""
     self.description.capitalize!
+  end
+
+  def fill_location
+    self.location = "It's a secret!" if description == ""
+    self.location.capitalize!
   end
 
 end
