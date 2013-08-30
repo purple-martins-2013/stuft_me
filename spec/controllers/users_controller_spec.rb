72c0729721cce5fff6aeb8ba8679fa972ba796bd
Apprentice <apprentice@dbc21.local>
2013-08-30 16:58:30 -0700

    make user controller specs for show and index actions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe UsersController do
 
+  let(:valid_user) { FactoryGirl.create(:user) }
+  let(:invalid_user) { FactoryGirl.build(:user) }
+
+  user2 = FactoryGirl.build(:user)
+  puts "*"*50
+  p user2
+
+  describe "GET #show" do
+    context "When User is invalid" do
+      it "should raise error" do
+       expect { get :show , :id => invalid_user.id }.to raise_error
+      end 
+    end
+
+    context "When User is valid" do
+      it "should render the #show view" do
+        get :show, id: valid_user.id
+        response.should render_template :show
+      end
+    end
+  end
+
+  describe "GET #index" do
+    it "should render the #index view" do
+      get :index
+      response.should render_template :index
+    end 
+  end
 end
+
