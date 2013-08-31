637687c032419ca5f53436db27f767897ee3caf8
Anirudh Dhullipalla <rudysback@gmail.com>
2013-08-31 16:48:15 -0700

    fixing local git master

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(username: "FirstUser")
+user = User.create(username: "FirstUser", uid: "stuff", provider: "yea")
+
+10.times do |index|
+	User.create(username: "FirstUser", uid: "stuff#{index}", provider: "yea#{index}")
+end
 
 40.times do |index|
   Plate.create(user: user, url: "http://gallery.photo.net/photo/175115#{sprintf '%02d', index}-md.jpg", description: "Once in a lifetime #{index}", location: "Walla Walla #{index}", price: "Bora Bora #{index}")
 end
+
