e2140a8c4d26ae27c2dd6f2fb098b389afbbddbb
Jeff Chang <jeffchang.ca@gmail.com>
2013-08-31 16:42:10 -0700

    add profile url and profile pic to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831223430) do
+ActiveRecord::Schema.define(version: 20130831232617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20130831223430) do
     t.datetime "updated_at"
     t.string   "provider"
     t.integer  "uid"
+    t.string   "profile_url"
+    t.string   "profile_pic"
   end
 
 end
