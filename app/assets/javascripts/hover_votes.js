7824bc47e521c3e39c25ec2f2ea8b741e3f010ad
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-01 16:03:15 -0700

    make mouse over js drool count reflect actual drool count, not hard coded anymore

--- app/assets/javascripts/hover_votes.js
+++ app/assets/javascripts/hover_votes.js
@@ -9,8 +9,15 @@ $(document).ready(function() {
 function HoverVoteable(container) {
   $(container).on('mouseover', '.index-photo', 
     function() {
+      var drool_count = $(this).data('drools');
       $(this).addClass("show-votes");
-      $(this).append("<h2 class='vote-text'>35 Votes</h2>");
+      if (drool_count == 1) {
+        $(this).append("<h2 class='vote-text'>"+drool_count+" Drool</h2>");
+      } 
+      else{
+        $(this).append("<h2 class='vote-text'>"+drool_count+" Drools</h2>");
+      }
+
     }).on("mouseleave", ".index-photo", function() {
       $(this).removeClass("show-votes");
       $(this).find(".vote-text").remove();
