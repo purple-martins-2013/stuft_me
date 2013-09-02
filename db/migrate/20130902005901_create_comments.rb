68824ae306d07ceeaa8b2da52064021d5af6ef1e
Jeff Chang <jeffchang.ca@gmail.com>
2013-09-01 17:59:32 -0700

    add comments table

---
+++ db/migrate/20130902005901_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.belongs_to :user, index: true
+      t.belongs_to :plate, index: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831232617) do
+ActiveRecord::Schema.define(version: 20130902005901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string  "content"
+    t.integer "user_id"
+    t.integer "plate_id"
+  end
+
+  add_index "comments", ["plate_id"], name: "index_comments_on_plate_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "drools", force: true do |t|
     t.integer  "user_id"
@@ -33,6 +42,20 @@ ActiveRecord::Schema.define(version: 20130831232617) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "drool_count", default: 0
+  end
+
+  create_table "plates_tags", force: true do |t|
+    t.integer "plate_id"
+    t.integer "tag_id"
+  end
+
+  add_index "plates_tags", ["plate_id"], name: "index_plates_tags_on_plate_id", using: :btree
+  add_index "plates_tags", ["tag_id"], name: "index_plates_tags_on_tag_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
