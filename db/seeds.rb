29ed454179593d2b885d706819ac01c1cbcb27a1
Jeff Chang <jeffchang.ca@gmail.com>
2013-09-02 01:02:22 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(username: "FirstUser", uid: "stuff", provider: "yea")
+SampleUsers = [
+  {username: "ashrod", provider: "instagram", uid: "3956455"},
+  {username: "cannellevanille", provider: "instagram", uid: "3619966"},
+  {username: "alxgrossmn", provider: "instagram", uid: "52757402"},
+  {username: "topwithcinnamon", provider: "instagram", uid: "32655024"},
+  {username: "julieskitchen", provider: "instagram", uid: "1357551"},
+  {username: "idafrosk", provider: "instagram", uid: "582820"},
+  {username: "aprilbloomfield", provider: "instagram", uid: "26494772"},
+  {username: "tracyshutterbean", provider: "instagram", uid: "28599339"},
+  {username: "davidlebovitz", provider: "instagram", uid: "2209896"},
+  {username: "sternmanrule", provider: "instagram", uid: "6247873"},
+  {username: "doriegreenspan", provider: "instagram", uid: "9960677"},
+  {username: "jamieoliver", provider: "instagram", uid: "3532778"},
+  {username: "eeddbbmm", provider: "instagram", uid: "1840409"},
+  {username: "thekitchn", provider: "instagram", uid: "386916469"},
+]
 
-10.times do |index|
-	User.create(username: "FirstUser", uid: "stuff#{index}", provider: "yea#{index}")
+SampleUsers.each do |sample_user|
+  User.create(sample_user)
 end
 
-40.times do |index|
-  Plate.create(user: user, url: "http://gallery.photo.net/photo/175115#{sprintf '%02d', index}-md.jpg", description: "Once in a lifetime #{index}", location: "Walla Walla #{index}", price: "Bora Bora #{index}")
+User.all.each do |user|
+  unique_instagram_urls_for(user).each do |url|
+    user.plates.create(url: url, drool_count: rand(0..975))
+  end
 end
-
